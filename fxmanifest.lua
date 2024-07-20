940cb9799637dce7ed70baa082ea0380988da51f
Apple <73302631+applesignal@users.noreply.github.com>
2024-07-20 17:03:56 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description '妞妞（撲克牌）模組'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+ui_page 'html/index.html'
+
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
