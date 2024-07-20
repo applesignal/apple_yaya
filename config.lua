809bc42c3615577ed5ddbaf16f08f2cadc48e0be
Apple <73302631+applesignal@users.noreply.github.com>
2024-07-20 17:07:18 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,18 @@
+-- config.lua
+
+Config = {}
+
+Config.MaxPlayers = 9
+Config.MinBankForDealer = 10000
+Config.TaxPercentage = 5
+Config.Multipliers = {
+    bull7 = 2,
+    bull8 = 2,
+    bull9 = 3,
+    bullbull = 5
+}
+
+Config.GameLocations = {
+    {x = 1111.11, y = 2222.22, z = 33.33},
+    {x = 4444.44, y = 5555.55, z = 66.66}
+}
